db9dca45e26ea371e0cdb29487fd708a2225960c
Aleksandr Matyka <o.matyka@flex-solution.com>
2022-04-03 22:41:40 +0300

    feat: add constants folder

---
+++ src/constants/constants.ts
@@ -0,0 +1,6 @@
+import { ProductIds } from "../pages/insurances-page/buyflow/Buyflow.types";
+
+export const PRODUCT_IDS_TO_NAMES = {
+  [ProductIds.devIns]: "Developer Insurance",
+  [ProductIds.desIns]: "Designer Insurance",
+};
---
+++ src/constants/messages.ts
@@ -0,0 +1,5 @@
+export const AGE_REQUIRED_MESSAGE = "Age is required";
+export const EMAIL_INVALID_MESSAGE = "Email is invalid";
+export const EMAIL_REQUIRED_MESSAGE = "Email is required";
+export const FIRST_NAME_REQUIRED_MESSAGE = "First name is required";
+export const LAST_NAME_REQUIRED_MESSAGE = "Last name is required";
