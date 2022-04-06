30d6fcf45e8b4dc8ece580a78d64991320d3013c
Aleksandr Matyka <o.matyka@flex-solution.com>
2022-04-06 04:12:55 +0300

    refactor(NotFound): change link with button link

--- src/pages/notFound-page/NotFound.tsx
+++ src/pages/notFound-page/NotFound.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from "react";
-import { Link } from "react-router-dom";
 import styles from "./notFound.module.scss";
-import { Button, Tag } from "../../components";
+import { Button, LinkButton, Tag } from "../../components";
 import { routes } from "../../routes/routes";
 
 export const NotFound: FC = (): JSX.Element => {
@@ -9,7 +8,7 @@ export const NotFound: FC = (): JSX.Element => {
     <div className={styles.notFoundWrapper}>
       <Tag tag="h2">Page not found</Tag>
       <Button type="button" className={styles.linkButton}>
-        <Link to={routes.INSURANCES_PAGE}>Back Home</Link>
+        <LinkButton to={routes.INSURANCES_PAGE}>Back Home</LinkButton>
       </Button>
     </div>
   );
